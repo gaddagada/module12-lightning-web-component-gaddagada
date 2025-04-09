9d63576bb13f2ffba1be62018f7f1c771fb2917c
gaddagada <gayathri.addagada@gmail.com>
2025-04-09 14:51:47 -0500

    Added variabes to store the score for each section of PD1 exam

--- force-app/main/default/lwc/platformDevCertCalculator/platformDevCertCalculator.js
+++ force-app/main/default/lwc/platformDevCertCalculator/platformDevCertCalculator.js
@@ -1,3 +1,13 @@
 import { LightningElement } from 'lwc';
 
-export default class PlatformDevCertCalculator extends LightningElement {}
+export default class PlatformDevCertCalculator extends LightningElement {
+
+    // Input Information variables 
+    devFundamentalScore = 50;
+    processAutomationScore = 50; 
+    userInterfaceScore = 50; 
+    testingScore = 50; 
+
+    //Certification score variable
+    certificationScore = 90; 
+}
