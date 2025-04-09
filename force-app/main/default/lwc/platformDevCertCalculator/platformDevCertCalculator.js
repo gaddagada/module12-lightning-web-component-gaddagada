0cba689e86f0bfac5a44d18393c2d3311240ee1a
gaddagada <gayathri.addagada@gmail.com>
2025-04-09 15:26:17 -0500

    have separation logic based on the input infroamtion and modify only the that field on the UI

--- force-app/main/default/lwc/platformDevCertCalculator/platformDevCertCalculator.js
+++ force-app/main/default/lwc/platformDevCertCalculator/platformDevCertCalculator.js
@@ -17,6 +17,21 @@ export default class PlatformDevCertCalculator extends LightningElement {
 
     handleChange(event){
         console.log(event);
-        console.log(event.target);
+        console.log(event.target.name, event.target.value);
+        console.log(event.target.type);
+        console.log(event.target.label);
+        const inputType = event.target.name; 
+        if(inputType === 'devFundamentals'){
+            this.devFundamentalScore = event.target.value;
+        }else if(inputType === 'processAutomationScore'){
+            this.processAutomationScore = event.target.value;
+        }if(inputType === 'userInterfaceScore'){
+            this.userInterfaceScore = event.target.value;
+        }if(inputType === 'testingScore'){
+            this.testingScore = event.target.value;
+        }
+    
+
+
     }
 }
